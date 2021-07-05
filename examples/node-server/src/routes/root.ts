0c020113157e42aad6aba43277a9373c32317215
Abhishek Kasireddy <akasireddy99@gmail.com>
2021-07-05 16:31:21 +0530

    pnp remove from .gitignore

--- examples/node-server/src/routes/root.ts
+++ examples/node-server/src/routes/root.ts
@@ -4,7 +4,7 @@ import { QuizClient } from "@qatapult/client";
 const router = express.Router();
 const quizzes = new QuizClient(
   // This key is fake
-  "c33380e02dfe259a9310a3861705b6481950b285e2f41b6b87b3968d6c40F361"
+  "c7a380e02dfe259a9310a3861705b6481950b285e2f41b6b87b3968d6c40536b"
 );
 
 router.post("/generate-quiz", async (req: any, res: any) => {
