03362768b891c0a9ab00941c317f5826f50763fe
Achuth Rajula <achuthrajula1999@gmail.com>
2021-07-07 17:11:47 +0530

    Intermediate commit -Achuth

--- examples/node-server/src/routes/root.ts
+++ examples/node-server/src/routes/root.ts
@@ -1,10 +1,12 @@
 import express from "express";
+const multer = require("multer");
+const upload = multer();
 import { QuizClient } from "@qatapult/client";
 
 const router = express.Router();
 const quizzes = new QuizClient(
   // This key is fake
-  "c7a380e02dfe259a9310a3861705b6481950b285e2f41b6b87b3968d6c40536b"
+  "438254d55f006536a8c48080c6419baa682c1d01e49e77707d787c589302fa9e"
 );
 
 router.post("/generate-quiz", async (req: any, res: any) => {
@@ -16,6 +18,40 @@ router.post("/generate-quiz", async (req: any, res: any) => {
 
   res.status(200).json({ ok: true, requestId });
 });
+
+router.post("/generate-quiz-text", async (req: any, res: any) => {
+  console.log(req.body);
+  const requestId = await quizzes.generateFromText(
+    req.body.url,
+    req.body.socketId
+  );
+
+  res.status(200).json({ ok: true, requestId });
+});
+
+router.post("/generate-quiz-yt", async (req: any, res: any) => {
+  console.log({ body: req.body });
+  const requestId = await quizzes.generateFromYouTube(
+    req.body.url,
+    req.body.socketId
+  );
+
+  res.status(200).json({ ok: true, requestId });
+});
+
+router.post(
+  "/generate-quiz-file",
+  upload.single("file"),
+  async (req: any, res: any) => {
+    console.log(req.file, typeof req.file);
+    const requestId = await quizzes.generateFromFile(
+      req.file,
+      req.body.socketId
+    );
+
+    res.status(200).json({ ok: true, requestId });
+  }
+);
 
 router.post("/completed-quiz", async (req, res) => {
   if (!req.body.ok) {
